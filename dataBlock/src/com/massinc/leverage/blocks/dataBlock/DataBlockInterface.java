dbe7bef257760b0a29e23ca5e7b28b481f3cd284
bhatia4 <kunal.bhatia78@gmail.com>
2014-08-24 00:18:19 -0400

    initial commit

---
+++ dataBlock/src/com/massinc/leverage/blocks/dataBlock/DataBlockInterface.java
@@ -0,0 +1,6 @@
+package com.massinc.leverage.blocks.dataBlock;
+
+public interface DataBlockInterface {
+	public Object getData(Object getParameters);
+	public Object setData(Object newData, Object getParameters);
+}
